07c43a6d67942e6bbf081bbce6cf11d96c89bbb4
phamtanphat <phatdroid94@gmail.com>
2022-01-22 18:56:52 +0700

    xu ly vi tri

--- app/src/main/java/com/example/apppickimage30112021/MainActivity2.java
+++ app/src/main/java/com/example/apppickimage30112021/MainActivity2.java
@@ -9,10 +9,14 @@ import android.widget.TableRow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MainActivity2 extends AppCompatActivity {
 
     TableLayout mTbLayout;
     String[] mArrNameImages;
+    int mPosition = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,7 @@ public class MainActivity2 extends AppCompatActivity {
         mTbLayout = findViewById(R.id.tableLayout);
 
         mArrNameImages = getResources().getStringArray(R.array.arr_image);
+        Collections.shuffle(Arrays.asList(mArrNameImages));
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -33,11 +38,13 @@ public class MainActivity2 extends AppCompatActivity {
             TableRow tableRow = new TableRow(this);
             for (int y = 0 ; y < 3 ; y++){
                 TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(width / 3,width / 3);
+                int resourceImage = getResources().getIdentifier(mArrNameImages[mPosition], "drawable", getPackageName());
                 ImageView imageView = new ImageView(this);
-                imageView.setImageResource(R.drawable.bo);
+                imageView.setImageResource(resourceImage);
                 layoutParams.gravity =  Gravity.CENTER;
                 imageView.setLayoutParams(layoutParams);
                 tableRow.addView(imageView);
+                mPosition += 1;
             }
             mTbLayout.addView(tableRow);
         }
